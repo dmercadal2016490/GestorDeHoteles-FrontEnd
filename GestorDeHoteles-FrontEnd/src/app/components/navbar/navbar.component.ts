8767f016840830d6cb81f2ddab5effd7941d1cea
squina2016297 <squina-2016297@kinal.edu.gt>
2021-05-11 18:37:35 -0600

    Navbar hecho

---
+++ GestorDeHoteles-FrontEnd/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RestUserService } from '../../services/restUser/rest-user.service';
+import { CONNECTION } from '../../services/global'
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit, DoCheck {
+  token: string;
+  user;
+  uri;
+
+  constructor(private router:Router, private restUser: RestUserService) { }
+
+  ngOnInit(): void {
+    this.token = localStorage.getItem('token');
+    this.user = JSON.parse(localStorage.getItem('user'));
+    this.uri = CONNECTION.URI;
+  }
+
+  ngDoCheck(){
+    this.token = this.restUser.getToken();
+    this.user = this.restUser.getUser();
+  }
+}
